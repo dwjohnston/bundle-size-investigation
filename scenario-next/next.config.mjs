ebe0ce1950daab4dc08b346c1a548f0c0d7239ee
David Johnston <david@blacksheepcode.com>
2024-09-17 12:00:02 +1000

    Generate all the files.

--- scenario-next/next.config.mjs
+++ scenario-next/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, options) => {
+
+        config.devServer.server = 'spdy';
+        return config; 
+    }
+};
 
 export default nextConfig;
