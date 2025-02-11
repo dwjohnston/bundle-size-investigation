dc61f8fbcef9d7a6a7d785da78fd3e59388b028d
David Johnston <david@blacksheepcode.com>
2025-02-11 22:37:41 +1100

    Update webpack settings

--- 6.5-webpack-only/webpack.config.js
+++ 6.5-webpack-only/webpack.config.js
@@ -2,23 +2,18 @@ import path from "path";
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-  
+
   entry: './src/index.mjs',
-  experiments: {
-    outputModule: true,
-  },
   output: {
     filename: '[name].js',
     path: path.resolve('dist'),
-    library: {
-      type: "module",
-    },
+    clean: true,
   },
   plugins: [new HtmlWebpackPlugin()],
   optimization: {
     splitChunks: {
       chunks: 'all',
-      minSize: 20000, 
+      minSize: 20000,
       maxSize: 50000,
     },
   },
